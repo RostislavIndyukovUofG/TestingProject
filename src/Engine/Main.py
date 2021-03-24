4fce2ae65f1550d255b4b0e28c00470593010a0b
RostislavUni <2538781I@student.gla.ac.uk>
2021-03-24 22:33:07 +0000

    Created command list display.

--- src/Engine/Main.py
+++ src/Engine/Main.py
@@ -13,7 +13,9 @@ class Main:
                 "add [game ID]": "add selected game to basket",
                 "basket": "view basket",
                 "remove [game ID]": "remove selected game from basket",
-                "buy": "purchase items in basket"}
+                "buy": "purchase items in basket",
+                "help": "show available commands",
+                "exit": "leave program"}
 
     def __init__(self, input_type):
         self.input_type = input_type
@@ -56,17 +58,50 @@ class Main:
         print("Welcome to the Game Store.")
         self.displayStock()
 
-    def displayCommandRow(self, key, value):
-        print(key,":\t", value)
+    def getLongestKeyLength(self):
+        longest_length = 0
+        for command_key in self.commands.keys():
+            key_length = len(command_key)
+
+            if key_length > longest_length:
+                longest_length = key_length
+        return longest_length
+
+
+    def displayCommandRow(self, key, value, command_key_length):
+        spacing_length = command_key_length - len(key)
+        spacing = " " * spacing_length
+        print(key + ": " + spacing + value)
+
 
     def displayCommands(self):
         print("The commands are:")
+        command_key_length = self.getLongestKeyLength()
+
         for command_row in self.commands.items():
-            self.displayCommandRow(command_row.key(), command_row.value())
+            self.displayCommandRow(command_row[0], command_row[1], command_key_length)
+        print()
 
+    def getUserCommand(self):
+        is_valid = False
+        while not is_valid:
+            try:
+                user_command = []
+                raw_command = input("Enter a command: ")
+                raw_command.strip()
+                user_command = raw_command.split(" ")
+                is_valid = True
+            except:
+                print("Invalid command, type help to see available commands: ")
+        return user_command
 
-    def getUserCommands(self):
+    def handleUserCommands(self):
         self.displayCommands()
+        active = True
+        while active:
+            user_command = self.getUserCommand()
+
+
 
 
 def main():
@@ -75,8 +110,7 @@ def main():
     main.setHeader(file_data[0])
     main.mapFileDataToGameData(file_data)
     main.displayInitialMessage()
-    user_command_string = getUserCommands()
-    handleCommand()
+    main.handleUserCommands()
 
 
 
