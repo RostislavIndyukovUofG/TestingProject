0d758bdae2ead2f42c2ce73f8cc97becc9df725c
RostislavUni <2538781I@student.gla.ac.uk>
2021-04-10 17:50:20 +0100

    Cleaned up CommandHandlerTest unit test methods.

--- test/EngineTests/CommandHandlerTest.py
+++ test/EngineTests/CommandHandlerTest.py
@@ -21,86 +21,86 @@ class CommandHandlerTest(unittest.TestCase):
     basket = command_handler.basket
 
     def test_handleValidListCommandOutput(self):
-        self.user_input.input_list = ["list", "exit"]
+        self.user_input.setInputList(["list", "exit"])
         close = self.command_handler.handleUserCommands()
         self.assertIn("Stock", self.user_output.getOutputList()[-2])
 
     def test_handleInvalidListCommandOutput(self):
         output_message = "Invalid command. Type help to see available commands."
 
-        self.user_input.input_list = ["list 1", "exit"]
+        self.user_input.setInputList(["list 1", "exit"])
         close = self.command_handler.handleUserCommands()
         self.assertEqual(output_message, self.user_output.getOutputList()[-2])
 
     def test_handleValidAddCommand(self):
-        self.user_input.input_list = ["add 1", "exit"]
+        self.user_input.setInputList(["add 1", "exit"])
         self.basket.setBasketList([])
         basket_size = len(self.basket.getBasketList())
         close = self.command_handler.handleUserCommands()
         self.assertEqual(basket_size + 1, len(self.basket.getBasketList()))
 
     def test_handleValidAddCommandOutput(self):
-        self.user_input.input_list = ["add 1", "exit"]
+        self.user_input.setInputList(["add 1", "exit"])
         self.basket.setBasketList([])
         close = self.command_handler.handleUserCommands()
         self.assertEqual("Game added successfully.", self.user_output.getOutputList()[-2])
 
     def test_handleInvalidAddCommand(self):
-        self.user_input.input_list = ["add", "exit"]
+        self.user_input.setInputList(["add", "exit"])
         self.basket.setBasketList([])
         basket_size = len(self.basket.getBasketList())
         close = self.command_handler.handleUserCommands()
         self.assertEqual(basket_size, len(self.basket.getBasketList()))
 
     def test_handleOutOfBoundsAddCommand(self):
-        self.user_input.input_list = ["add -1", "exit"]
+        self.user_input.setInputList(["add -1", "exit"])
         self.basket.setBasketList([])
         basket_size = len(self.basket.getBasketList())
         close = self.command_handler.handleUserCommands()
         self.assertEqual(basket_size, len(self.basket.getBasketList()))
 
     def test_handleInvalidAddCommandOutput(self):
-        self.user_input.input_list = ["add", "exit"]
+        self.user_input.setInputList(["add", "exit"])
         self.basket.setBasketList([])
         close = self.command_handler.handleUserCommands()
         output_message = "Invalid command. Type help to see available commands."
         self.assertEqual(output_message, self.user_output.getOutputList()[-2])
 
     def test_handleValidRemoveCommand(self):
-        self.command_handler.basket.updateBasket(self.game_data.getGameDataList()[0], "add")
-        self.user_input.input_list = ["remove 1", "exit"]
+        self.basket.updateBasket(self.game_data.getGameDataList()[0], "add")
+        self.user_input.setInputList(["remove 1", "exit"])
         basket_size = len(self.basket.getBasketList())
         close = self.command_handler.handleUserCommands()
         self.assertEqual(basket_size - 1, len(self.basket.getBasketList()))
 
     def test_handleValidRemoveCommandOutput(self):
         self.basket.updateBasket(self.game_data.getGameDataList()[0], "add")
-        self.user_input.input_list = ["remove 1", "exit"]
+        self.user_input.setInputList(["remove 1", "exit"])
         close = self.command_handler.handleUserCommands()
         self.assertEqual("Game removed successfully.", self.user_output.getOutputList()[-2])
 
     def test_handleInvalidRemoveCommand(self):
-        self.user_input.input_list = ["remove 1", "exit"]
+        self.user_input.setInputList(["remove 1", "exit"])
         self.basket.setBasketList([])
         basket_size = len(self.basket.getBasketList())
         close = self.command_handler.handleUserCommands()
         self.assertEqual(basket_size, len(self.basket.getBasketList()))
 
     def test_handleInvalidRemoveCommandOutput(self):
-        self.user_input.input_list = ["remove 1", "exit"]
+        self.user_input.setInputList(["remove 1", "exit"])
         self.basket.setBasketList([])
         close = self.command_handler.handleUserCommands()
         self.assertEqual("Game not in basket.", self.user_output.getOutputList()[-2])
 
     def test_handleOutOfBoundsRemoveCommandOutput(self):
-        self.user_input.input_list = ["remove -1", "exit"]
+        self.user_input.setInputList(["remove -1", "exit"])
         self.basket.setBasketList([])
         close = self.command_handler.handleUserCommands()
         output_message = "Invalid game id. Type list to view the available games."
         self.assertEqual(output_message, self.user_output.getOutputList()[-2])
 
     def test_handleValidBasketCommandOutput(self):
-        self.user_input.input_list = ["basket", "exit"]
+        self.user_input.setInputList(["basket", "exit"])
         self.basket.setBasketList([])
         self.basket.calcualateBasketTotal()
         close = self.command_handler.handleUserCommands()
@@ -108,63 +108,63 @@ class CommandHandlerTest(unittest.TestCase):
 
     def test_handleValidPurchaseCommand(self):
         self.basket.updateBasket(self.game_data.getGameDataList()[0], "add")
-        self.user_input.input_list = ["purchase", "exit"]
+        self.user_input.setInputList(["purchase", "exit"])
         close = self.command_handler.handleUserCommands()
         output_message = "\nThank you for your purchase!\n"
         self.assertIn(output_message, self.user_output.getOutputList()[-2])
 
     def test_handleValidPurchaseCommandWithEmptyBasketOutput(self):
-        self.user_input.input_list = ["purchase", "exit"]
+        self.user_input.setInputList(["purchase", "exit"])
         self.basket.setBasketList([])
         close = self.command_handler.handleUserCommands()
         self.assertEqual("Your basket is emtpy.", self.user_output.getOutputList()[-2])
 
     def test_handleInvalidPurchaseCommand(self):
-        self.user_input.input_list = ["purchase 1", "exit"]
+        self.user_input.setInputList(["purchase 1", "exit"])
         basket_size = len(self.basket.getBasketList())
         close = self.command_handler.handleUserCommands()
         self.assertEqual(basket_size, len(self.basket.getBasketList()))
 
     def test_handleInvalidPurchaseCommandOutput(self):
-        self.user_input.input_list = ["purchase 1", "exit"]
+        self.user_input.setInputList(["purchase 1", "exit"])
         close = self.command_handler.handleUserCommands()
         output_message = "Invalid command. Type help to see available commands."
         self.assertEqual(output_message, self.user_output.getOutputList()[-2])
 
     def test_handleValidHelpCommandOutput(self):
-        self.user_input.input_list = ["help", "exit"]
+        self.user_input.setInputList(["help", "exit"])
         close = self.command_handler.handleUserCommands()
         self.assertIn("leave program", self.user_output.getOutputList()[-2])
 
     def test_handleInvalidHelpCommandOutput(self):
-        self.user_input.input_list = ["help 1", "exit"]
+        self.user_input.setInputList(["help 1", "exit"])
         close = self.command_handler.handleUserCommands()
         output_message = "Invalid command. Type help to see available commands."
         self.assertIn(output_message, self.user_output.getOutputList()[-2])
 
     def test_handleValidExitCommandOutput(self):
-        self.user_input.input_list = ["exit"]
+        self.user_input.setInputList(["exit"])
         close = self.command_handler.handleUserCommands()
         self.assertIn("Closing program.", self.user_output.getOutputList()[-1])
 
     def test_handleInvalidExitCommandOutput(self):
-        self.user_input.input_list = ["exit 1", "exit"]
+        self.user_input.setInputList(["exit 1", "exit"])
         close = self.command_handler.handleUserCommands()
         output_message = "Invalid command. Type help to see available commands."
         self.assertIn(output_message, self.user_output.getOutputList()[-2])
 
     def test_handleValidCommandAlternative(self):
-        self.user_input.input_list = ["e"]
+        self.user_input.setInputList(["e"])
         close = self.command_handler.handleUserCommands()
         self.assertIn("Closing program.", self.user_output.getOutputList()[-1])
 
     def test_handleValidCommandWithSpaces(self):
-        self.user_input.input_list = ["      exit      "]
+        self.user_input.setInputList(["      exit      "])
         close = self.command_handler.handleUserCommands()
         self.assertIn("Closing program.", self.user_output.getOutputList()[-1])
 
     def test_handleMultipleCommandsWithSpaces(self):
-        self.user_input.input_list = ["ad 1", "add 1", " buy", "e"]
+        self.user_input.setInputList(["ad 1", "add 1", " buy", "e"])
         close = self.command_handler.handleUserCommands()
         self.assertIn("Closing program.", self.user_output.getOutputList()[-1])
 
