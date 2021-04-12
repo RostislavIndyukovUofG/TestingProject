670ad4aa56e7e84318af1c12c9a0acd294f61085
RostislavUni <2538781I@student.gla.ac.uk>
2021-04-12 10:44:17 +0100

    Cleaned up faking test

--- test/DisplayTests/ConsoleInputTest.py
+++ test/DisplayTests/ConsoleInputTest.py
@@ -11,15 +11,14 @@ class ConsoleInputTest(unittest.TestCase):
     input_list = LogFileReader.readFromLogFile("../../resources/IOLogs/InputLog.txt")
     output_list = LogFileReader.readFromLogFile("../../resources/IOLogs/OutputLog.txt")
 
-    main = Main(DataInputFile(), TestInput(), TestOutput())
-    main.user_input.setInputList(input_list)
-    main.user_output.setOutputList(output_list)
-
     def test_userInputFaking(self):
-        self.main.initialiseHelperClasses()
-        self.main.displayInitialMessage()
-        close = self.main.command_handler.handleUserCommands()
-        self.assertEqual(self.output_list, self.main.user_output.output_list)
+        main = Main(DataInputFile(), TestInput(), TestOutput())
+        main.user_input.setInputList(self.input_list)
+        main.user_output.setOutputList(self.output_list)
+        main.initialiseHelperClasses()
+        main.displayInitialMessage()
+        close = main.command_handler.handleUserCommands()
+        self.assertEqual(self.output_list, main.user_output.output_list)
 
 
 if __name__ == '__main__':
