24643429dba352536c9eb6e2acc437ff3f15133d
RostislavUni <2538781I@student.gla.ac.uk>
2021-04-12 10:21:13 +0100

    Cleaned up MainTest

--- test/EngineTests/MainTest.py
+++ test/EngineTests/MainTest.py
@@ -9,25 +9,15 @@ from src.Engine.Main import Main
 
 
 class MainTest(unittest.TestCase):
-    input_type = DataInputStub()
     user_input = ConsoleInput(False)
     user_output = TestOutput()
-    main = Main(input_type, user_input, user_output)
-
-    data_list = [['GameID', 'Game Name', 'Price', 'Stock'],
-                 ['1', 'City Builder', '8.99', '4'],
-                 ['2', 'Survivalist', '9.99', '5']]
-
-    def test_setGameDataMock(self):
-        DataInputStub.getFileData = MagicMock(return_value=self.data_list)
-        self.main.setGameData()
-        self.assertEqual(2, len(self.main.game_data.getGameDataList()))
 
     def test_displayInitialMessage(self):
-        self.main.displayInitialMessage()
+        main = Main(DataInputStub(), self.user_input, self.user_output)
+        main.displayInitialMessage()
         self.assertEqual("Welcome to the Game Store.", self.user_output.getOutputList()[-1])
 
-    def test_setDataFromFoundFile(self):
+    def test_setGameDataFromFoundFile(self):
         main = Main(DataInputFile(), self.user_input, self.user_output)
         main.setFilePath("../../resources/gameData.csv")
         main.setGameData()
@@ -35,26 +25,31 @@ class MainTest(unittest.TestCase):
         game_name = game_data_list[0].getGameName()
         self.assertEqual('City Builder', game_name)
 
-    def test_setDataFromMissingFile(self):
+    def test_setGameDataFromMissingFile(self):
         main = Main(DataInputFile(), self.user_input, self.user_output)
         main.setFilePath("missing file")
         main.setGameData()
         output_message = "An error occurred when reading the file. Switching to stub data."
         self.assertEqual(output_message, self.user_output.output_list[-1])
 
-    def test_setDataFromEmptyFile(self):
+    def test_setGameDataFromEmptyFile(self):
         main = Main(DataInputFile(), self.user_input, self.user_output)
         main.setFilePath("../../resources/emptyTestFile.csv")
         main.setGameData()
         output_message = "An error occurred when reading the file. Switching to stub data."
         self.assertEqual(output_message, self.user_output.output_list[-1])
 
-    def test_setDataFromStub(self):
+    def test_setGameDataFromStub(self):
         main = Main(DataInputStub(), self.user_input, self.user_output)
         main.setGameData()
         game_data_list = main.game_data.getGameDataList()
         game_name = game_data_list[0].getGameName()
         self.assertEqual("X-Destroyer Video Game", game_name)
+
+    def test_setGameDataFromStubListLength(self):
+        main = Main(DataInputStub(), self.user_input, self.user_output)
+        main.setGameData()
+        self.assertEqual(3, len(main.game_data.getGameDataList()))
 
 
 if __name__ == '__main__':
